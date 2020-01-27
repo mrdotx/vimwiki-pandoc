9888b1a4cdca9d4d7f864debf9af01d062b3f3e7
mrdotx <klassiker@gmx.de>
2020-01-27 12:04:42 +0100

    typos corrected

--- copy.sh
+++ copy.sh
@@ -3,20 +3,20 @@
 # path:       ~/projects/vimwiki-pandoc/copy.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/vimwiki-pandoc
-# date:       2020-01-22T12:31:24+0100
+# date:       2020-01-27T11:57:39+0100
 
 notes="$HOME/Dokumente/Notes/html"
 
 # copy to webservers
 echo "Copy to hermes..."
-if ping -c1 -W1 -q hermes > /dev/null 2>&1; then
+if ping -c1 -W1 -q hermes >/dev/null 2>&1; then
     rsync --info=progress2 --delete -acL "$notes"/ alarm@hermes:/srv/http/notes/
 else
     echo "Can not copy files, hermes is not available!"
 fi
 
 echo "Copy to prometheus..."
-if ping -c1 -W1 -q prometheus > /dev/null 2>&1; then
+if ping -c1 -W1 -q prometheus >/dev/null 2>&1; then
     rsync --info=progress2 --delete -acL "$notes"/ alarm@prometheus:/srv/http/notes/
 else
     echo "Can not copy files, prometheus is not available!"
