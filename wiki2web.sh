1fd1a3aed51d09913f3dc9603e375ba6f37274b0
mrdotx <klassiker@gmx.de>
2026-05-07 05:33:21 +0200

    folder variables replaced by dir

--- wiki2web.sh
+++ wiki2web.sh
@@ -3,10 +3,10 @@
 # path:   /home/klassiker/.local/share/repos/vimwiki-pandoc/wiki2web.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/vimwiki-pandoc
-# date:   2026-03-31T05:34:17+0200
+# date:   2026-05-07T05:32:47+0200
 
-source_folder="$HOME/Documents/Notes/html/"
-destination_folder="/srv/http/wiki"
+source_dir="$HOME/Documents/Notes/html/"
+destination_dir="/srv/http/wiki"
 
 # color variables for the interactive shell
 tty -s \
@@ -18,23 +18,23 @@ tty -s \
 sync_to() {
     rsync -acLvh --delete \
         --exclude prinzipal.html \
-        "$source_folder" "${1:+"$1:"}$destination_folder"
+        "$source_dir" "${1:+"$1:"}$destination_dir"
 }
 
 case $@ in
     "")
         printf "%b%b::%b %bsync%b %b%s%b %bto%b %b%s%b\n" \
             "$bold" "$blue" "$reset" "$bold" "$reset" \
-            "$cyan" "$source_folder" "$reset" "$bold" "$reset" \
-            "$cyan" "$destination_folder" "$reset"
+            "$cyan" "$source_dir" "$reset" "$bold" "$reset" \
+            "$cyan" "$destination_dir" "$reset"
         sync_to
         ;;
     *)
         for host_name in "$@"; do
             printf "%b%b::%b %bsync%b %b%s%b %bto%b %b%s%b %bon%b %b%s%b\n" \
             "$bold" "$blue" "$reset" "$bold" "$reset" \
-                "$cyan" "$source_folder" "$reset" "$bold" "$reset" \
-                "$cyan" "$destination_folder" "$reset" "$bold" "$reset" \
+                "$cyan" "$source_dir" "$reset" "$bold" "$reset" \
+                "$cyan" "$destination_dir" "$reset" "$bold" "$reset" \
                 "$cyan" "$host_name" "$reset"
             sync_to "$host_name"
         done
